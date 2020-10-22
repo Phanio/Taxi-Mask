6254c84ec067054fd598c63471bcafd783213bf2
Benoît SCHULER <schuler@et.intechinfo.fr>
2020-10-22 11:22:02 +0200

    25 Add dummy test

--- taxi-mask-backend/src/test/java/fr/et/intechinfo/mousqinfos/taximask/AppTests.java
+++ taxi-mask-backend/src/test/java/fr/et/intechinfo/mousqinfos/taximask/AppTests.java
@@ -3,11 +3,14 @@ package fr.et.intechinfo.mousqinfos.taximask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class AppTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
